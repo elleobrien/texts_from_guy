7f9fc8d9de6526e26241ace76d5a263f13f552a0
Elle O'Brien <andronovhopf@gmail.com>
2020-04-27 12:27:28 -0700

    Update textmemaybe.py

--- textmemaybe.py
+++ textmemaybe.py
@@ -1,11 +1,18 @@
 from twilio.rest import Client
+import os
 
-client = Client('ACCOUNT_SID', 'AUTH_TOKEN')
+account_sid = os.getenv('ACCOUNT_SID')
+auth_token = os.getenv('AUTH_TOKEN')
+from_num = os.getenv('TWILIO_NO')
+to_num = os.getenv('MY_NO')
+                
+
+client = Client(account_sid, auth_token)
 
 message = client.messages \
     .create(
          body='HELP HELP',
-         from_='TWILIO_NO',
-         to='MY_NO'
+         from_=from_num,
+         to=to_num
      )
 
