ae6a1ae7a3838e9cdd3ff3e12f67fccfa48d76a8
Elle O'Brien <andronovhopf@gmail.com>
2020-04-27 11:45:29 -0700

    Create textmemaybe.py

---
+++ textmemaybe.py
@@ -0,0 +1,12 @@
+from twilio.rest import Client
+
+client = Client('ACCOUNT_SID', 'AUTH_TOKEN')
+
+message = client.messages \
+    .create(
+         body='HELP HELP',
+         from_='TWILIO_NO',
+         to='MY_NO'
+     )
+
+print(message.sid)
